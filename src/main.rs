f2a8f56686e77d3051b56a3dd163977b170b453c
Damian Trojnar <trojnar.damian@gmail.com>
2021-01-31 11:14:29 +0100

    Add basic REPL

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use std::io;
+
+// https://github.com/kanaka/mal#mal---make-a-lisp
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello in MAL sandbox!");
+    let mut line : String = String::from("");
+    while line != "exit\n" {
+        line = rep()
+    }
 }
+
+fn read() -> String {
+    let mut input = String::new();
+    match io::stdin().read_line(&mut input) {
+        Ok(_n) => {
+            input
+        }
+        Err(_error) =>  "".to_string()
+    }
+}
+
+fn eval(line: &str) -> String {
+  return line.to_string();
+}
+
+fn print(ast: &str){
+    println!("{}", ast);
+}
+
+fn rep() -> String {
+    let line = read();
+    let ast = eval(&line);
+    print(&ast);
+    return line;
+}
+    
